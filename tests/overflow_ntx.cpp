677e29e27b4902dba8b55e3ae74f9658f852a6ff
Kartal Kaan Bozdoğan <kartalkaanbozdogan@gmail.com>
2021-06-13 23:47:08 +0300

    Added tests for non-tx operations Updated the pmdk fork

---
+++ tests/overflow_ntx.cpp
@@ -0,0 +1,36 @@
+#include <libpmemobj.h>
+#include <iostream>
+#include <assert.h>
+#include <unistd.h>
+
+POBJ_LAYOUT_BEGIN(spmo_test);
+POBJ_LAYOUT_ROOT(spmo_test, struct root);
+POBJ_LAYOUT_TOID(spmo_test, struct dummy);
+POBJ_LAYOUT_END(spmo_test);
+
+struct root {
+	TOID(struct dummy) obj;
+};
+
+struct dummy {
+	uint64_t x[2];
+};
+
+int main()
+{
+	unlink("spmo_test.pool");
+	PMEMobjpool* pool = pmemobj_create("spmo_test.pool", "spmo_test", 32*1024*1024, 0660);
+	assert(pool != NULL);
+
+	PMEMoid proot_ = pmemobj_root(pool, sizeof(struct root));
+	assert(OID_IS_NULL(proot_) == false);
+	struct root* proot = (struct root*)pmemobj_direct(proot_);
+	assert(0 == POBJ_NEW(pool, &proot->obj, struct dummy, NULL, NULL));
+	
+	D_RW(proot->obj)->x[0] = 1;
+	D_RW(proot->obj)->x[1] = 2;
+	D_RW(proot->obj)->x[2] = 3; // This line should crash
+	
+	pmemobj_close(pool);
+	return 0;
+}
---
+++ tests/use_after_free_ntx.cpp
@@ -0,0 +1,35 @@
+#include <libpmemobj.h>
+#include <iostream>
+#include <assert.h>
+#include <unistd.h>
+
+POBJ_LAYOUT_BEGIN(spmo_test);
+POBJ_LAYOUT_ROOT(spmo_test, struct root);
+POBJ_LAYOUT_TOID(spmo_test, struct dummy);
+POBJ_LAYOUT_END(spmo_test);
+
+struct root {
+	TOID(struct dummy) obj;
+};
+
+struct dummy {
+	uint64_t x;
+};
+
+int main()
+{
+	unlink("spmo_test.pool");
+	PMEMobjpool* pool = pmemobj_create("spmo_test.pool", "spmo_test", 32*1024*1024, 0660);
+	assert(pool != NULL);
+
+	PMEMoid proot_ = pmemobj_root(pool, sizeof(struct root));
+	assert(OID_IS_NULL(proot_) == false);
+	struct root* proot = (struct root*)pmemobj_direct(proot_);
+	assert(0 == POBJ_NEW(pool, &proot->obj, struct dummy, NULL, NULL));
+	TOID(struct dummy) toid = proot->obj; // The FREE call sets proot->obj to NULL, so we create a copy here.
+	POBJ_FREE(&proot->obj);
+	D_RW(toid)->x = 1; // This line should crash
+	
+	pmemobj_close(pool);
+	return 0;
+}
