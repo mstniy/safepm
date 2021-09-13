69a4c057be0f82ed3bb71c8ae817919b8352f1c9
Kartal Kaan Bozdoğan <kartalkaanbozdogan@gmail.com>
2021-09-13 18:03:57 +0200

    pmemkv benchmark: Removed the repeat loop from run-variants.sh, as the individual driver scripts do repetitions and averaging

--- benchmarks/pmemkv/run-variants.sh
+++ benchmarks/pmemkv/run-variants.sh
@@ -7,15 +7,10 @@ do
   docker build -t pmemkv-bench-$variant -f=Dockerfile_$variant .
 done
 
-REPEATS=3
-
 mkdir -p results
 
-for i in $(seq 1 $REPEATS)
+for variant in "${variants[@]}"
 do
-  for variant in "${variants[@]}"
-  do
-    mkdir results/${variant}_$i
-    docker run -v $(pwd)/results/${variant}_$i:/results -it --shm-size=2g pmemkv-bench-$variant ./run-all.sh randomreadwrite
-  done
+  mkdir results/${variant}
+  docker run -v $(pwd)/results/${variant}:/results -it --shm-size=2g pmemkv-bench-$variant ./run-all.sh
 done
