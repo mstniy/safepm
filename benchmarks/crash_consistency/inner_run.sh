85efb18163bf6765fa3387f90b7766d6181438b8
dimstav23 <dimstav23@gmail.com>
2022-01-31 22:03:44 +0000

    update memcheck & pmemcheck for crash consistency -- redirect output to files

--- benchmarks/crash_consistency/inner_run.sh
+++ benchmarks/crash_consistency/inner_run.sh
@@ -2,11 +2,11 @@ set -e
 
 cd /pmdk_wrappers_only/src/benchmarks
 echo "----- CRASH CONSISTENCY MAP BENCHMARK WITH MEMCHECK -----"
-ASAN_OPTIONS=detect_leaks=0 LD_LIBRARY_PATH=../nondebug valgrind --tool=memcheck ./pmembench pmembench_map.cfg
+ASAN_OPTIONS=detect_leaks=0 LD_LIBRARY_PATH=../nondebug valgrind --log-file="/results/map_memcheck" --tool=memcheck ./pmembench pmembench_map.cfg
 echo "----- CRASH CONSISTENCY MAP BENCHMARK WITH PMEMCHECK -----"
-ASAN_OPTIONS=detect_leaks=0 LD_LIBRARY_PATH=../nondebug valgrind --tool=pmemcheck ./pmembench pmembench_map.cfg
+ASAN_OPTIONS=detect_leaks=0 LD_LIBRARY_PATH=../nondebug valgrind --log-file="/results/map_pmemcheck" --tool=pmemcheck ./pmembench pmembench_map.cfg
 
 echo "----- CRASH CONSISTENCY TX BENCHMARK WITH MEMCHECK -----"
-LD_LIBRARY_PATH=../debug valgrind --tool=memcheck ./pmembench pmembench_tx_safepm.cfg
+ASAN_OPTIONS=detect_leaks=0 LD_LIBRARY_PATH=../nondebug valgrind --log-file="/results/tx_memcheck" --tool=memcheck ./pmembench pmembench_tx_safepm.cfg
 echo "----- CRASH CONSISTENCY TX BENCHMARK WITH PMEMCHECK -----"
-ASAN_OPTIONS=detect_leaks=0 LD_LIBRARY_PATH=../debug valgrind --tool=pmemcheck ./pmembench pmembench_tx_safepm.cfg
+ASAN_OPTIONS=detect_leaks=0 LD_LIBRARY_PATH=../nondebug valgrind --log-file="/results/tx_pmemcheck" --tool=pmemcheck ./pmembench pmembench_tx_safepm.cfg
--- benchmarks/crash_consistency/run-variants.sh
+++ benchmarks/crash_consistency/run-variants.sh
@@ -6,5 +6,7 @@ then
   exit 1
 fi
 
+mkdir -p results
+
 docker build -t safepm-crash_consistency .
-docker run -v "$BENCHMARK_PM_PATH:/mnt/crash_consistency" -it safepm-crash_consistency bash inner_run.sh
+docker run -v "$BENCHMARK_PM_PATH:/mnt/crash_consistency" -v "$(pwd)/results":/results -t safepm-crash_consistency bash inner_run.sh
